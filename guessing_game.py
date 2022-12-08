6e9323e2cbc80b51c540a9da6bf49dc1fca8ba85
maxnhill <hillmax@umich.edu>
2022-12-08 19:05:33 +0000

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -8,30 +8,32 @@ For this first project we will be using Workspaces.
 NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
 
 """
-
 import random
+#Display Intro Message
+print("Welcome to Maximilian's guessing game, lets have some fun!")
+#Store a random number between 1 and 10
+answer= random.randint(1,10)
+num_guess=0
+guess= int(input("Please choose a number between 1 and 10: ")) 
+#Initiate While loop
+while guess!= answer:
+    num_guess +=1
+    #Institute Try Block
+    #try:
+        #Prompt user for choosen number
+    #except ValueError:
+        #print("Oops. Looks like you didnt enter a number. Try again!")
+    #if guess is to high
+    if guess == answer:
+        print(f"{answer} was the correct answer. It took you {num_guess} attempt(s)")
 
-
-def start_game():
-    """Psuedo-code Hints
+    elif guess < answer < 10:
+        #print go lower
+        print("Looks like you need to go lower")    
+        
+    else:
+        #print go higher
+        print("Looks like you need to go higher")
+        
+print("Thank youfor playing Maximilian's guessing game!")
     
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-    
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-    
-    ( You can add more features/enhancements if you'd like to. )
-    """
-    # write your code inside this function.
-
-
-
-# Kick off the program by calling the start_game function.
-start_game()
